591f88eea6b4c4a50b1010671a04ef2a04d10f69
Alkhairi Jusuf <1stezza199@gmail.com>
2026-02-24 10:44:57 +0700

    push

--- src/components/Pengalaman.jsx
+++ src/components/Pengalaman.jsx
@@ -135,7 +135,7 @@ const Pengalaman = () => {
     <section
       id="pengalaman"
       ref={sectionRef}
-      className="relative scroll-mt-16 overflow-x-hidden"
+      className="relative scroll-mt-16"
     >
       <div
         className={`relative w-full py-16 md:py-24 px-6
