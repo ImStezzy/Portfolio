6e32a6a55a41d8d1dc4a9ff6e64de008994315fd
Alkhairi Jusuf <1stezza199@gmail.com>
2026-02-24 10:33:35 +0700

    push

--- src/components/Pengalaman.jsx
+++ src/components/Pengalaman.jsx
@@ -49,7 +49,7 @@ const slides = [
     cardLogo: logoMAB,
     card: {
       description:
-        "PT Mitra Abadi Bahari merupakan perusahaan distribusi FMCG yang berafiliasi dengan DMLT Group.",
+        "PT Mitra Abadi Bahari merupakan perusahaan distribusi yang berafiliasi dengan DMLT Group.",
     },
   },
   {
@@ -181,21 +181,24 @@ const Pengalaman = () => {
               ))}
             </div>
 
-            {/* DOT NAV SMALL FIX */}
-              <div className="flex gap-2 mt-4 ml-2 items-center">
-                {slides.map((_, index) => (
-                  <button
-                    key={index}
-                    onClick={() => scrollTo(index)}
-                    className={`rounded-full flex-shrink-0 aspect-square transition-all duration-200
-                    ${
-                      selectedIndex === index
-                        ? "w-2 h-2 bg-black"
-                        : "w-2 h-2 bg-gray-400 opacity-40"
-                    }`}
-                  />
-                ))}
-              </div>
+            {/* DOT NAV BULAT FIX */}
+            <div className="flex gap-2 mt-4 ml-2 items-center">
+              {slides.map((_, index) => (
+                <button
+                  key={index}
+                  onClick={() => scrollTo(index)}
+                  className={`
+                    w-[6px] h-[6px] 
+                    min-w-[6px] min-h-[6px]
+                    aspect-square
+                    rounded-full
+                    flex-shrink-0
+                    transition-opacity duration-200
+                    ${selectedIndex === index ? "bg-black opacity-100" : "bg-gray-400 opacity-40"}
+                  `}
+                />
+              ))}
+            </div>
            </div> 
 
           {/* CARD */}
