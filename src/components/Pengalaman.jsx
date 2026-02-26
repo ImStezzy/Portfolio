5dc79366616daa3e06833e8c000beff3a6b9fa81
Alkhairi Jusuf <1stezza199@gmail.com>
2026-02-26 09:08:11 +0700

    update

--- src/components/Pengalaman.jsx
+++ src/components/Pengalaman.jsx
@@ -138,7 +138,7 @@ const Pengalaman = () => {
       className="relative scroll-mt-16"
     >
       <div
-        className="relative w-full py-20 md:py-28 px-6 bg-[#16151b]"
+        className="relative w-full py-20 md:py-28 px-6 bg-[#0b0b0f]"
       >
 
         <motion.div
